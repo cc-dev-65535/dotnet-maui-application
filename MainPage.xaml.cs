d551f398208cf53b917d6f09a0bf72a0138b5860
Collin Chan <omiyage4949@gmail.com>
2022-12-22 00:14:56 -0800

    Work on main page layout

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,17 +1,32 @@
-﻿namespace Scratch
+﻿using Scratch.Models;
+using System.Collections.ObjectModel;
+
+namespace Scratch
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        public ObservableCollection<NoteItem> Notes { get; set; } = new();
+        //int count = 0;
 
         public MainPage()
         {
             InitializeComponent();
+            NotesCollectionView.ItemsSource = Notes;
+
+            Init();
+        }
+
+        private void Init()
+        {
+            var note = new NoteItem();
+            note.Text = "Hi";
+            note.CreatedDate = DateTime.Now;
+            Notes.Add(note);
         }
 
         private void OnCounterClicked(object sender, EventArgs e)
         {
-            count++;
+            //count++;
             //CounterLabel.Text = $"Current count: {count}";
 
             //SemanticScreenReader.Announce(CounterLabel.Text);
