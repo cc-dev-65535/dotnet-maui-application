3a728cc53601ac910df9ff438e079092836b9445
Collin Chan <omiyage4949@gmail.com>
2022-12-23 20:22:46 -0800

    Add database libraries

---
+++ Data/Database.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Scratch.Models;
+using SQLite;
+
+namespace Scratch.Data
+{
+    public class Database
+    {
+
+    }
+}
